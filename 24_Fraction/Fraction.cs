fced051116d18360eaa6669b77a37add7b518263
ml <132607150+Martinus110410@users.noreply.github.com>
2025-11-07 11:23:28 +0100

    1

---
+++ 24_Fraction/Fraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Fraction
+{
+    internal class Fraction
+    {
+        // Properties 
+        public int Nominator { get; set; }
+        public int DeNominator { get; set; }
+
+        public Fraction(int Nominator, int DeNominator)
+        {
+            this.Nominator = Nominator;
+            this.DeNominator = DeNominator;
+        }
+
+        public string GetFraction()
+        {
+            return $"{this.Nominator} / {this.DeNominator}";
+        }
+
+        public double GetValue()
+        {
+            return (double)Nominator / (double)DeNominator;
+        }
+    }
+}
