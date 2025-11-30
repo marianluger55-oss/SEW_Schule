fcb2f547734dd8474b0d7d573baae9e1e19a19a9
MarianLuger <marian.luger@icloud.com>
2025-11-30 19:26:50 +0100

    1

---
+++ 09_Christmas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09_Christmas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte gib ein Jahr ein: ");
+            string eingabe = Console.ReadLine();
+            int jahr;
+
+            if (int.TryParse(eingabe, out jahr))
+            {
+                Christmas.PrintHolyNightsOnSunday(jahr, jahr);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl ein.");
+            }
+        }
+    }
+}
