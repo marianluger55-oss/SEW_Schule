6be8eea429be8e90b85410956d2e731d641369dd
ml <132607150+Martinus110410@users.noreply.github.com>
2025-12-05 10:21:37 +0100

    Stopwatch

---
+++ 09_StopWatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using _09_StopWatch;
+using System.Diagnostics;
+using System.Timers;
+
+
+
+
+Console.WriteLine("Bitte eine taste drücken.");
+
+Console.ReadKey();
+StopWatch keyOne = new StopWatch();
+
+
+
+while (Console.KeyAvailable)
+
+{
+
+    Console.ReadKey(true);
+
+    //oder
+
+    Console.WriteLine("Ungültig. ): ");
+
+    return;
+
+}
+
+Console.Clear();
+
+Console.WriteLine("Bitte noch eine Taste drücken.");
+
+Console.ReadKey();
+
+double elapsed = keyOne.ElapsedSec(); 
+
+Console.WriteLine($"Zeit: {elapsed}s");
+
+
+
+
+
+
---
+++ 09_StopWatch/StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_StopWatch
+{
+    internal class StopWatch
+    {
+        private DateTime start; 
+
+        public StopWatch() 
+        {
+            this.start = DateTime.Now;    
+        }
+
+        public double ElapsedSec()
+        {
+            return (DateTime.Now - start).TotalSeconds;
+
+        }
+
+        public void Reset()
+        {
+            this.start = DateTime.Now; 
+        }
+    }
+}
+
+// Bsp.: ReactionTime mit Stopwatch
+
+
